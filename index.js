344ee942db166a304f1135380da6bdef9ea0e16a
D9dman <tawhid268@gmail.com>
2023-06-15 09:12:02 +0600

    added delete functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Defining Port
 const port = process.env.PORT || 5000;
@@ -27,15 +27,38 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+
+    // Database Collections
+    const taskCollection = client.db("taskManager").collection("tasks");
+    const usersCollection = client.db("taskManager").collection("users");
+
+    app.get("/alltasks/:email", async (req, res) => {
+      const email = req.params.email;
+
+      console.log(email);
+
+      const query = { email: email };
+      const result = await taskCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    app.delete("/deletetask/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.post("/addtask", async (req, res) => {
+      const doc = req.body;
+      const result = await taskCollection.insertOne(doc);
+      res.send(result);
+    });
+
+    console.log("Connected to MongoDB.");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
